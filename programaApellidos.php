8b120022211c49f6d2af213e00490d1f6bf8c0a8
emmavillegas <Emmanuel.villegas@est.fi.uncoma.edu.ar>
2023-11-13 16:35:56 -0300

    agregue function del menu

--- programaApellidos.php
+++ programaApellidos.php
@@ -31,6 +31,25 @@ function cargarColeccionPalabras()
     return ($coleccionPalabras);
 }
 
+function solicitarNumeroEntre($min, $max)
+{
+    //int $numero
+
+    $numero = trim(fgets(STDIN));
+
+    if (is_numeric($numero)) { //determina si un string es un número. puede ser float como entero.
+        $numero  = $numero * 1; //con esta operación convierto el string en número.
+    }
+    while (!(is_numeric($numero) && (($numero == (int)$numero) && ($numero >= $min && $numero <= $max)))) {
+        echo "Debe ingresar un número entre " . $min . " y " . $max . ": ";
+        $numero = trim(fgets(STDIN));
+        if (is_numeric($numero)) {
+            $numero  = $numero * 1;
+        }
+    }
+    return $numero;
+}
+
 /* ****COMPLETAR***** */
 
 

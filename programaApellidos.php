5c22d7fe897199796db42ca861631dab67f00a9f
emmavillegas <Emmanuel.villegas@est.fi.uncoma.edu.ar>
2023-11-14 16:51:25 -0300

    borramos funcion ya declarada

--- programaApellidos.php
+++ programaApellidos.php
@@ -31,25 +31,6 @@ function cargarColeccionPalabras()
 
     return ($coleccionPalabras);
 }
-
-function solicitarNumeroEntre($min, $max)
-{
-    //int $numero
-
-    $numero = trim(fgets(STDIN));
-
-    if (is_numeric($numero)) { //determina si un string es un número. puede ser float como entero.
-        $numero  = $numero * 1; //con esta operación convierto el string en número.
-    }
-    while (!(is_numeric($numero) && (($numero == (int)$numero) && ($numero >= $min && $numero <= $max)))) {
-        echo "Debe ingresar un número entre " . $min . " y " . $max . ": ";
-        $numero = trim(fgets(STDIN));
-        if (is_numeric($numero)) {
-            $numero  = $numero * 1;
-        }
-    }
-    return $numero;
-}
 function menu(){
     echo"╔════════════════════════════════════════════════════════════════════════════════╗\n";
     echo"║ seleccione una opcion:                                                         ║\n";
